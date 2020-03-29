a82d7a910e0c240d140fa1755418034dfebb834b
Nicolas Toledo <nicolas.toledo@fatec.sp.gov.br>
2020-03-29 00:51:21 -0300

    Back-end e Front-end do app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './global.css';
+
+//JSX (JavaScript XML)
+//Um componente no React é uma função que retorna HTML
+//import Header from './Header';
+import Routes from './routes';
+
+function App() {
+ 
+
+    return (
+      <Routes/>  
+  );
+}
+
+export default App;
